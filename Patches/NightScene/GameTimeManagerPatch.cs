fc238186892c264dcdaa26c2198c79a7544cf70d
MetaMiku <MetaMiku@hotmail.com>
2026-04-30 16:32:14 +0800

    fix: remove useless OnPlayableDirectorPlayed_Prefix (ref: #60)

--- Patches/NightScene/GameTimeManagerPatch.cs
+++ Patches/NightScene/GameTimeManagerPatch.cs
@@ -1,16 +1,11 @@
 using HarmonyLib;
 
-using SgrYuki;
-
 namespace MetaMystia.Patch;
 
 [HarmonyPatch(typeof(Common.TimelineExtestion.GameTimeManager))]
 [AutoLog]
 public partial class GameTimeManagerPatch
 {
-    public const string StoryStartActionKey = "story_start_disable_collision";
-    public const string StoryStopActionKey = "story_over_enable_collision";
-
     [HarmonyPatch(nameof(Common.TimelineExtestion.GameTimeManager.SetGameTimeMode))]
     [HarmonyPrefix]
     public static void SetGameTimeMode_Prefix(Common.TimelineExtestion.GameTimeManager __instance, ref Common.TimelineExtestion.GameTimeManager.TimeMode mode)
@@ -24,41 +19,4 @@ public partial class GameTimeManagerPatch
 
     public static void FreezeTime() => Common.TimelineExtestion.GameTimeManager.Instance?.SetGameTimeMode(Common.TimelineExtestion.GameTimeManager.TimeMode.Freeze);
     public static void ResumeTime() => Common.TimelineExtestion.GameTimeManager.Instance?.SetGameTimeMode(Common.TimelineExtestion.GameTimeManager.TimeMode.Resume);
-
-    [HarmonyPatch(nameof(Common.TimelineExtestion.GameTimeManager.OnPlayableDirectorPlayed))]
-    [HarmonyPrefix]
-    public static void OnPlayableDirectorPlayed_Prefix(Common.TimelineExtestion.GameTimeManager __instance, UnityEngine.Playables.PlayableDirector playableDirector)
-    {
-        var onStopped = (UnityEngine.Playables.PlayableDirector _) =>
-        {
-            CommandScheduler.RemoveKeyFromKeyQueue(StoryStartActionKey);
-
-            CommandScheduler.EnqueueKey(
-                key: StoryStartActionKey,
-                executeWhen: () => PlayerManager.Peer?.GetCharacterUnit() != null,
-                execute: () =>
-                {
-                    Log.Info($"story over, enabled collision for {PlayerManager.Peer?.GetCharacterUnit()?.name}");
-                    PlayerManager.EnablePeerCollision(true);
-                }
-            );
-            playableDirector.remove_stopped(_onPlayableDirectorStoppedCallback);
-            Log.Info($"removed callback for {playableDirector.name}");
-        };
-        _onPlayableDirectorStoppedCallback = onStopped;
-        playableDirector.add_stopped(_onPlayableDirectorStoppedCallback);
-
-        CommandScheduler.RemoveKeyFromKeyQueue(StoryStopActionKey);
-        CommandScheduler.EnqueueKey(
-            key: StoryStartActionKey,
-            executeWhen: () => PlayerManager.Peer?.GetCharacterUnit() != null,
-            execute: () =>
-            {
-                Log.Info($"story start, disabled collision for {PlayerManager.Peer?.GetCharacterUnit()?.name}");
-                PlayerManager.EnablePeerCollision(false);
-            }
-        );
-    }
-
-    private static Il2CppSystem.Action<UnityEngine.Playables.PlayableDirector> _onPlayableDirectorStoppedCallback;
 }
